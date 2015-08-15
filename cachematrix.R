100c9ca2e876b255bb59ba6c455bb112efb4e446
mosm <mosm@me.com>
2015-08-15 10:50:42 +0200

    Solution
    Added makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,48 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# makeCacheMatrix creates a special "matrix", which is really a list containing functions to:
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the inverse of the matrix
+# 4. get the inverse of the matrix
+# This works in conjunction with cacheSolve. If the inverse has already been calculated, it will be retrieved through getinverse
+# getinverse will return NULL if the inverse is not yet known
+# if the content of the matrix is altered, the inverse is reset to NULL
+makeCacheMatrix <- function(mm = matrix()) {
+  # Set inverse to NULL on creation
+  i <- NULL;
+  set <- function(m) {
+    mm <<- m
+    # set inverse to NULL in in case of matrix change
+    i <<- NULL
+  }
+  get <- function() mm
+    
+  # Cache inverse for future use
+  setinverse <- function(si) i <<- si
+  # Retrieve cached inverse (or NULL in case inverse has not been set)
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+# cacheSolve calculated the inverse of the special "matrix" created with makeCacheMatrix(). It first checks if the inverse has 
+# already been calculated. If that is the case, the computation is skipped and the inverse is returned. 
+# Otherwise, the inverse is calculated and retained using the cache via the setinverse() function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Check if inverse has been cached  
+  i <- x$getinverse()
+  # If inverse has been cached (i.e. != NULL) return cached inverse
+  if (!is.null(i)) {
+    return(i)
+  }
+  # get matrix
+  data <- x$get()
+  # calculated inverse
+  i <- solve(data)
+  # Cache inverse to prevent recalculation
+  x$setinverse(i)
+  i
 }
